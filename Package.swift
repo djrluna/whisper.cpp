9086b57461b8ba7861010af3cb5ea6ef84a7c855
djrluna@gmail.com <djrluna@gmail.com>
2024-01-29 09:22:05 -0500

    .

--- Package.swift
+++ Package.swift
@@ -43,7 +43,6 @@ let package = Package(
                 .unsafeFlags(["-Wno-shorten-64-to-32", "-O3", "-DNDEBUG"]),
                 .define("GGML_USE_ACCELERATE"),
                 .unsafeFlags(["-fno-objc-arc"]),
-                .define("WHISPER_USE_COREML", .when(platforms: [.macOS, .macCatalyst, .iOS])),
                 .define("GGML_USE_METAL")
                 //.define("WHISPER_COREML_ALLOW_FALLBACK")
                 // NOTE: NEW_LAPACK will required iOS version 16.4+
